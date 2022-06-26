b93b799b7cb042cdcb978e91008622c89ca43919
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2022-06-26 18:59:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+  Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+const lista = ['Agustin', 22, true,"28-05-2000",{
+    titulo: 'El extraño caso del doctor Jekyll y el señor Hyde',
+    autor: 'Robert Louis Stevenson',
+    fecha:'5 de enero de 1886',
+    url:'https://www.amazon.com/-/es/Robert-Louis-Stevenson/dp/1490926585'
+
+}]
+
+
